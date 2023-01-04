7d53dbf9c70cc711f6ebd974068c595e55012300
Legion <Legion>
2023-01-04 18:06:55 -0500

    Add Intake Motor Relay

--- src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.Subsystems;
 
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Relay.Value;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
@@ -8,7 +10,8 @@ import frc.robot.RobotMap;
  * 
  */
 public class IntakeSubsystem extends SubsystemBase {
-  
+
+  private final Relay intakeRelay = new Relay(0);
 
   private Spark 
     intakeBottom= RobotMap.intakeBottom, 
@@ -20,8 +23,22 @@ public class IntakeSubsystem extends SubsystemBase {
       intakeTop.set(-.65);
     
   }
+  //-1, Reverse; 0, Off; 1 Forward
+  public void reLaySetDirection(int d){
+if(d== -1){
+  intakeRelay.set(Value.kReverse);
+}
+if(d== 0){
+  intakeRelay.set(Value.kOff);
+}
+if(d== 1){
+  intakeRelay.set(Value.kForward);
+}
+
+  }
 
   
+
   public void intakeBottom(double speed) {
     // Turn on motor for intake bottom
       intakeBottom.set(speed);    
