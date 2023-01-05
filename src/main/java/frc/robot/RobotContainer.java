52114267865f379c8d1dcbc2e30ced623698c416
Acer1 <Acer1>
2023-01-05 18:43:13 -0500

    Update so it moves forward and back and intakes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,9 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Subsystems.*;
@@ -159,7 +162,9 @@ public class RobotContainer {
     //     m_autoCommand = auto1;
     //     break;
     // }
-    return new SequentialCommandGroup(pidF, new WaitCommand(3),pidB);
+    // return new SequentialCommandGroup(new ParallelRaceGroup(pidF, intakeBottom), new WaitCommand(2), new ParallelRaceGroup(pidB, reverse));
+    return new SequentialCommandGroup(new ParallelDeadlineGroup(pidF, intakeBottom), new InstantCommand(intakeSub::stopIntakes), new WaitCommand(2), pidB);
+
     
   }
 }
