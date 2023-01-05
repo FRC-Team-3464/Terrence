304c8530925ef0875edc5c9d37b41f32ea4875d2
Acer1 <Acer1>
2023-01-05 17:52:12 -0500

    create backwards command

---
+++ src/main/java/frc/robot/Commands/pidBackCMD.java
@@ -0,0 +1,51 @@
+package frc.robot.Commands;
+
+import frc.robot.Subsystems.DriveSubsystem;
+import frc.robot.Subsystems.EncoderSubsystem;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+   * Enables tank drive with 2 joysticks
+   *
+   * The subsystem used by this command.
+   */
+
+public class pidBackCMD extends CommandBase {
+  //@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final PIDController driverPID = new PIDController(0.015625, 0, 0);
+//   private final PIDCommand driveFowardPID = new PIDCommand(driverPID, measurementSource, setpointSource, useOutput, requirements);
+private final DriveSubsystem driveSub;
+private final EncoderSubsystem encoderSub;
+  public pidBackCMD(DriveSubsystem drive, EncoderSubsystem encode) {
+      driveSub = drive;
+      encoderSub = encode;
+      addRequirements(driveSub);
+      addRequirements(encoderSub);
+  }
+
+  @Override
+  public void initialize() {
+        
+  }
+  @Override
+  public void execute() {
+    driveSub.arcadeDrive(-(driverPID.calculate(encoderSub.getRightEncoder(), -32)), 0);
+  }
+
+
+  @Override
+  public void end(boolean interrupted){
+    encoderSub.resetEncoders();
+  }
+
+ @Override
+  public boolean isFinished() {
+    if(Math.abs(driverPID.getPositionError()) < 8){
+        return true;
+    }
+    else{
+        return false;
+  }}
+
+}
