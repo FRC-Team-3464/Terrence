ef3d6427f89d57d61f0c33c06a9a26859c43154c
Legion <Legion>
2023-01-31 19:50:37 -0500

    work on balancing

--- src/main/java/frc/robot/Commands/tiltPIDCmd.java
+++ src/main/java/frc/robot/Commands/tiltPIDCmd.java
@@ -4,6 +4,9 @@
 
 package frc.robot.Commands;
 
+import java.util.ArrayList;
+import java.util.concurrent.DelayQueue;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -14,13 +17,20 @@ import frc.robot.Subsystems.NavX;
 public class tiltPIDCmd extends CommandBase {
   /** Creates a new tiltPIDCmd. */
 
-  private final PIDController balanceController = new PIDController(-0.025, 0, 0);
+  // private final PIDController balanceController = new PIDController(-0.025, 0, 0);
   
   private final NavX navX;
   private final DriveSubsystem driveSubsystem;
   private double outputSpeed;
   private boolean status;
 
+  // private double[] past5Errors; 
+  private double testCaseNumber = 0; // testCaseNumber increases by one if it's on the ramp -> used to help set the ramp angle. 
+  private boolean isOn;
+  // private double rampAngle;
+  private double robotAngle;
+  private double changeInError;
+  ArrayList<Double> deltaX = new ArrayList<Double>();
   public tiltPIDCmd(NavX navSub, DriveSubsystem driveSub) {
     navX = navSub;
     driveSubsystem = driveSub;
@@ -32,26 +42,46 @@ public class tiltPIDCmd extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    balanceController.setSetpoint(0); // We need 
+    // balanceController.setSetpoint(0); // We need 
     // balanceController.setTolerance(2);
     outputSpeed = 0;
-    status  = true; // True is green - good
+    isOn = false;
+    status  = false; // True is green - good
+    robotAngle = navX.returnPitch();
+    double angle2 = navX.returnPitch();
+    deltaX.add(angle2-robotAngle);
+    // Get angle to be the navX reading
+    // perviousError = 0;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    outputSpeed = balanceController.calculate(navX.returnPitch());
-    if((Math.abs(outputSpeed) > 0.5) || (Math.abs(outputSpeed) < 0.13125)){
-      outputSpeed = 0;
-      status = false;
-    }else{
-      status = true;      
-    }
+    changeInError = navX.returnPitch() - deltaX.get(deltaX.size()-1);
+    if (!(changeInError - deltaX.get(deltaX.size()-1) > 2))
+    deltaX.add(changeInError);
+    // Take the difference between the navX value now than before. 
+    
 
-    // driveSubsystem.arcadeDrive(outputSpeed, 0); // Just drive forward and back. 
-    SmartDashboard.putNumber("Balance Speed", outputSpeed);
-    SmartDashboard.putBoolean("Balance Status", status); // RED IS NOT GOOD
+
+
+    
+      if(changeInError < 0 ){
+         driveSubsystem.arcadeDrive(-0.25, 0);
+      }
+      else if(changeInError > .05 && deltaX.get(deltaX.size()-2) >0.05 )
+      {
+         driveSubsystem.arcadeDrive(0, 0);
+        status = true;
+      }  // driveSubsystem
+   
+    robotAngle = navX.returnPitch();
+    SmartDashboard.putNumber("Error", deltaX.get(deltaX.size()-1));
+    SmartDashboard.putNumber("Change in error", changeInError);
+    SmartDashboard.putNumber("Balance Speed", outputSpeed); 
+    SmartDashboard.putBoolean("On Ramp", isOn); // RED IS NOT GOOD
+    SmartDashboard.putBoolean("Completed", status); // RED IS NOT GOOD
+   
   }
 
   // Called once the command ends or is interrupted.
